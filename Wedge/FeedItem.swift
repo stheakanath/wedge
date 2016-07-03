0f9dfe19b2bf6d20d941677dae47c7930117d38b
Sony Theakanath <stheakanath@gmail.com>
2016-07-03 13:18:31 -0400

    first commit'

---
+++ Wedge/FeedItem.swift
@@ -0,0 +1,19 @@
+//
+//  FeedItem.swift
+//  Iron
+//
+//  Created by Kuriakose Sony Theakanath on 7/3/16.
+//  Copyright © 2016 Kuriakose Sony Theakanath. All rights reserved.
+//
+
+import UIKit
+
+class FeedItem: UITableViewCell {
+    @IBOutlet weak var eventName: UILabel!
+    @IBOutlet weak var eventPicture: UIImageView!
+    @IBOutlet weak var eventPrice: UILabel!
+    @IBOutlet weak var eventThing: UILabel!
+    
+    
+    
+}
