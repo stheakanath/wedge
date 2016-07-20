9671ea952a38cabea0bcb2e99b6ded8d65be407f
Gavin <gavinmai@stanford.edu>
2016-07-19 22:24:43 -0400

    Still working on profile - tab bar view controller is weird

---
+++ Wedge/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Wedge
+//
+//  Created by Gavin Mai on 7/19/16.
+//  Copyright © 2016 Kuriakose Sony Theakanath. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        self.tableView.contentInset = UIEdgeInsetsMake(20, 0, 0, 0)
+    }
+    
+}
