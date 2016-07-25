dce3695aa7977a5cd2ae9707f30936ea482e2a4f
Kuriakose Sony Theakanath <ktheakanath@apple.com>
2016-07-25 10:49:13 -0700

    SS

---
+++ Wedge/SpotifyLoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SpotifyLoginViewController.swift
+//  Wedge
+//
+//  Created by Kuriakose Theakanath on 7/25/16.
+//  Copyright © 2016 Kuriakose Sony Theakanath. All rights reserved.
+//
+
+import UIKit
+
+class SpotifyLoginViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
